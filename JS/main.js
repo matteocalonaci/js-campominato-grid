b778418996436e5e1cda0a76fbe6378d803632b9
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-04-30 15:54:50 +0200

    MILESTONE #1

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// MILESTONE #1
+// Create la vostra griglia in HTML e CSS, in maniera grossolana.
+// Vi servirà solo per testare le classi, come visto in mattinata con la griglia 3x3.
+
+
+// MILESTONE #2
+// Commentate l'HTML e iniettatelo tramite un ciclo in JS, senza pensare al click o alle funzioni.
+
+
+// MILESTONE #3
+// Aggiungete la gestione dell'evento di click, che stampi il numero e cambi il colore della cella.
+
+
+// MILESTONE #4
+// Spostate alcuni elementi in funzioni, ad esempio la creazione del quadrato e/o l'event listener.
---
+++ readme.md
@@ -0,0 +1,11 @@
+MILESTONE #1
+- creare una griglia in HTML CSS.
+
+MILESTONE #2
+- commento quanto fatto in HTML e lo replico in javaScript.
+
+MILESTONE #3
+- vado ad inserire un evento sui quadrati che al click mi cambi il colore.
+
+MILESTONE #4
+- sposto ciò che mi serve della MILESTONE #3 in una funzione.
