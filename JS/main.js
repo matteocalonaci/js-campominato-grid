a7e64b01526c9cf74612e2a2f22c45298be807fc
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-04-30 16:16:44 +0200

    MILESTONE #2

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,31 @@
 
 // MILESTONE #4
 // Spostate alcuni elementi in funzioni, ad esempio la creazione del quadrato e/o l'event listener.
+
+// _________________________________________________________________________________________________
+
+
+//vado a recuperarmi la mia griglia in HTML e lo salvo in una variabile
+let griglia = document.getElementById("griglia")
+
+//onde evitare di di gestire 100 quadrati in HTML, procediamo creando un ciclo FOR
+//che va a stamparmi ad ogni giro di ciclo un quadrato <=100.
+
+for (i = 1; i <= 100; i++) {
+
+    //vado a creare una classe div equivalente ai quadrati in HTML e la salvo in una variabile.
+    let quadrato = document.createElement("div");
+
+    // aggiungo a quadrato la class di CSS.
+    quadrato.classList.add("quadrato");
+
+    //inserisco un numero progressivo a quadrato
+    quadrato.innerText = i;
+
+    //adesso aggiungo quadrato alla griglia
+    griglia.append(quadrato);
+
+
+}
+
+
