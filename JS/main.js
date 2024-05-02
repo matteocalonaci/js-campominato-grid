0bba5a1d12fa61d84bad618792ab9397684bc921
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-02 15:05:47 +0200

    Bonus 2

--- JS/main.js
+++ JS/main.js
@@ -18,29 +18,38 @@
 
 
 //vado a recuperarmi la mia griglia in HTML e lo salvo in una variabile
-let griglia = document.getElementById("griglia")
+
+
+// let griglia = document.getElementById("griglia")
 
 //onde evitare di di gestire 100 quadrati in HTML, procediamo creando un ciclo FOR
 //che va a stamparmi ad ogni giro di ciclo un quadrato <=100.
 
-for (i = 1; i <= 100; i++) {
+// for (i = 1; i <= 100; i++) {
 
-    // *vado a creare una classe div equivalente ai quadrati in HTML e la salvo in una variabile.
+// *vado a creare una classe div equivalente ai quadrati in HTML e la salvo in una variabile.
 
-    // **  aggiungo a quadrato la class di CSS.
+// **  aggiungo a quadrato la class di CSS.
 
-    // *** inserisco un numero progressivo a quadrato
+// *** inserisco un numero progressivo a quadrato
 
-    //(MILESTONE #4) creo una nuova varibile che mi vada a richiamare la funzione creaQuadrato
-    let elemento = creaQuadrato(i);
+//(MILESTONE #4) creo una nuova varibile che mi vada a richiamare la funzione creaQuadrato
 
-
-    //adesso aggiungo quadrato alla griglia
-    griglia.append(elemento);
-}
+// let elemento = creaQuadrato(i);
 
 
-//fuori dal ciclo vado a creare una funzione 
+//adesso aggiungo quadrato alla griglia
+// griglia.append(elemento);
+// }
+
+// let button = document.querySelector("btn")
+
+// button.addEventListener("click", (){
+
+
+// })
+
+//fuori dal ciclo vado a creare una funzione
 // cos'è una cosa che ripetiamo nel ciclo FOR? --> il creare quadrato
 // prendiamo quindi la formala e la inseriamo in nella funzione.
 // che poi andremo a richiamare all'interno del ciclo.
@@ -57,9 +66,10 @@ function creaQuadrato(contenuto) {
 
     // **** (MILESTONE #3) adesso vado ad aggiungere ad ogni quadrato un evento clic 
     quadrato.addEventListener("click", function () {
-        console.log("cliccato")
+        console.log("selezionato", contenuto)
         //aggiungo la classe eveidenziata  per i quadrati che vado a cliccare
         quadrato.classList.toggle("evidenziato")
+
 
         //uso toggle e non add perchè toggle mi permette in automatico di aggiungere la classe dove non c'è e toglierla dove c'è al solo click
 
@@ -74,7 +84,28 @@ function creaQuadrato(contenuto) {
 
 }
 
+//BONUS 2
+let button = document.getElementById("btn")
+button.addEventListener("click", function () {
+    griglia.innerHTML = ""
 
+    for (i = 1; i <= 100; i++) {
+
+        // *vado a creare una classe div equivalente ai quadrati in HTML e la salvo in una variabile.
+
+        // **  aggiungo a quadrato la class di CSS.
+
+        // *** inserisco un numero progressivo a quadrato
+
+        //(MILESTONE #4) creo una nuova varibile che mi vada a richiamare la funzione creaQuadrato
+        let elemento = creaQuadrato(i);
+
+
+        //adesso aggiungo quadrato alla griglia
+        griglia.append(elemento);
+
+    }
+})
 
 
 
